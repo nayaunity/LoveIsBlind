aeecad4e8f3337103ab3a8668360e8bc71236bb9
NyaradzoB <79171484+NyaradzoB@users.noreply.github.com>
2024-03-12 17:36:43 -0600

    initial commit

---
+++ LoveIsBlind/LoveIsBlind/LoveIsBlindApp.swift
@@ -0,0 +1,34 @@
+//
+//  LoveIsBlindApp.swift
+//  LoveIsBlind
+//
+//  Created by Nyaradzo Bere on 3/12/24.
+//
+
+import SwiftUI
+import FirebaseCore
+
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+  func application(_ application: UIApplication,
+                   didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+    FirebaseApp.configure()
+
+    return true
+  }
+}
+
+@main
+struct LoveIsBlindApp: App {
+  // register app delegate for Firebase setup
+  @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+
+
+  var body: some Scene {
+    WindowGroup {
+      NavigationView {
+        ContentView()
+      }
+    }
+  }
+}
